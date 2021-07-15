bf8fa866b0a15412a978c40bf51b10c271c73861
MedhaShrivastava <medhakumari299@gmail.com>
2021-07-15 14:34:39 +0530

    2 files modified, 1 added

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,12 +3,13 @@ import '../../App.css';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 
+import About from '../About';
 
 function Home() {
   return (
     <>
       <HeroSection />
-    
+      <About />
       <Footer />
     </>
   );
