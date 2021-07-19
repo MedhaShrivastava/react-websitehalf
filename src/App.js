8418037a562755d89b0eb5ae877212f0d9d350f5
MedhaShrivastava <medhakumari299@gmail.com>
2021-07-19 14:54:05 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   (async () => {
     try {
       await appID.init({
-        clientId:'< >',
+        clientId:'<c5f19698-a4e4-4e98-97c7-7fc96b0c95a3>',
         discoveryEndpoint: '<https://eu-gb.appid.cloud.ibm.com/oauth/v4/5c0dc695-4ee3-45e4-9139-14874b29b5d5/.well-known/openid-configuration>'
       });
     } catch (e) {
