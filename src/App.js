a06f925f3ba834df5bbeda7aacb009a8e23cf689
MedhaShrivastava <medhakumari299@gmail.com>
2021-07-19 16:15:00 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,66 +1,25 @@
 
 import React from 'react';
 import './App.css';
-import AppID from 'ibmcloud-appid-js';
-
+import Home from './components/pages/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
 
 function App() {
-  const appID = React.useMemo(() => {
-    return new AppID()
-  }, []);
-
-  const [errorState, setErrorState] = React.useState(false);
-  const [errorMessage, setErrorMessage] = React.useState('');
-
-  (async () => {
-    try {
-      await appID.init({
-        clientId:'<c5f19698-a4e4-4e98-97c7-7fc96b0c95a3>',
-        discoveryEndpoint: '<https://eu-gb.appid.cloud.ibm.com/oauth/v4/5c0dc695-4ee3-45e4-9139-14874b29b5d5/.well-known/openid-configuration>'
-      });
-    } catch (e) {
-      setErrorState(true);
-      setErrorMessage(e.message);
-    }
-  })();
-
-  const [welcomeDisplayState, setWelcomeDisplayState] = React.useState(false);
-  const [loginButtonDisplayState, setLoginButtonDisplayState] = React.useState(true);
-  const [userName, setUserName] = React.useState('');
-
-  const loginAction = async () => {
-    try {
-      const tokens = await appID.signin();
-      setErrorState(false);
-      setLoginButtonDisplayState(false);
-      setWelcomeDisplayState(true);
-      setUserName(tokens.idTokenPayload.name);
-    } catch (e) {
-      setErrorState(true);
-      setErrorMessage(e.message);
-    }
-  };
-
-
   return (
-
-<div className="App">
-      <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
-        {welcomeDisplayState && <div> Welcome {userName}! You are now authenticated.</div>}
-        {loginButtonDisplayState &&
-        <button
-            style={{fontSize: '24px', backgroundColor: 'skyblue', border: 'none'}}
-            id='login'
-            onClick={loginAction}>Login
-        </button>
-        }
-        {errorState && <div style={{color: 'red'}}>{errorMessage}</div>}
-      </header>
-    </div>
- 
-  ) ;
-      }
-
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/services' component={Services} />
+          <Route path='/products' component={Products} />
+        </Switch>
+      </Router>
+    </>
+  );
+}
 
 export default App;
